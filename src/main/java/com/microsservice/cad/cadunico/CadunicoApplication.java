4ac9d21f89c12819b4b1cabd127601c7ad1f209b
maiconsflorencio <mflorencio-ext@farmtech.com.br>
2022-12-18 16:27:17 -0300

    primeiro commit do projeto

---
+++ src/main/java/com/microsservice/cad/cadunico/CadunicoApplication.java
@@ -0,0 +1,13 @@
+package com.microsservice.cad.cadunico;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CadunicoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CadunicoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/microsservice/cad/cadunico/CadunicoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.microsservice.cad.cadunico;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CadunicoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
