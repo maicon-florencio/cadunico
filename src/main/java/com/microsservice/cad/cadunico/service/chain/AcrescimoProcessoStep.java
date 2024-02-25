355bdd34476273d0a2cbf05cece3b7d9025d07f1
maiconsflorencio <mflorencio-ext@farmtech.com.br>
2024-02-24 21:00:38 -0300

    anotacoes e remocao de importes nao usados

--- src/main/java/com/microsservice/cad/cadunico/service/chain/AcrescimoProcessoStep.java
+++ src/main/java/com/microsservice/cad/cadunico/service/chain/AcrescimoProcessoStep.java
@@ -1,13 +1,25 @@
 package com.microsservice.cad.cadunico.service.chain;
 
-import lombok.Getter;
-
-
+/**
+ * Classe abtract que representa a corrente que será indicada a parti dela.
+ *
+ */
 public abstract class AcrescimoProcessoStep {
     protected AcrescimoProcessoStep nextStep;
     protected Object[] args;
 
     public AcrescimoProcessoStep(Object... args) { this.args = args; }
+
+    /**
+     *
+     * @param context
+     * @return
+     * Class AcrescimoProcessContext representada no processo como objeto a ser verificado
+     * esse metodo abtract será implementado nas class que estenderem AcrescimoProcessoStep
+     * pois passam a ser classe concreta e aplicaram as regra de processamento necessario
+     * sempre desconhecendo a proxima chama que fizerem.
+     * @throws Exception
+     */
     public abstract AcrescimoProcessContext execute(AcrescimoProcessContext context)throws Exception;
 
     public void setNextStep(AcrescimoProcessoStep next){ this.nextStep = next;}
